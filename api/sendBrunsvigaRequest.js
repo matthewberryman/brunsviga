815a986729f7211f8b438f8078bec823c039cc8f
Matthew Berryman <matthew@acrossthecloud.net>
2018-04-06 14:12:27 +1000

    Various changes: * Use promises server side. * Validation of expression. * Client side handling for invalid expressions.

--- api/sendBrunsvigaRequest.js
+++ api/sendBrunsvigaRequest.js
@@ -1,38 +1,58 @@
 console.log('Loading function');
 
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
+
+const valid = function(expression) {
+
+  const operators = /[\+-/\*]/;
+
+  return operators.test(expression) &&  // must have an operator
+    expression.split(operators).length === 2 && // must have two operands
+    expression.split(operators)[0].length <= 10 && /\d+/.test(expression.split(operators)[0]) && // operands must be numbers of length < 10
+    expression.split(operators)[1].length <= 10 && /\d+/.test(expression.split(operators)[1]);
+
+}
 
 exports.handler = function(event, context, callback) {
-    console.log("\n\nLoading handler\n\n");
-    var sns = new AWS.SNS();
-    var requestBody = JSON.parse(event.body);
+  console.log("\n\nLoading handler\n\n");
+  var sns = new AWS.SNS();
+  var requestBody = JSON.parse(event.body);
+  if (valid(requestBody.expression)) {
     sns.publish({
-        Message: 'Send result of ' + requestBody.expression + ' to ' + requestBody.email,
-        TopicArn: process.env.TOPIC_ARN
-    }, function(err, data) {
-        if (err) {
-            console.log(err.stack);
-            const response = {
-              statusCode: 503,
-              headers: {
-              "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-              "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-            },
-            body: JSON.stringify({ "message": "Server error " + err.stack })
-          };
-          callback(null, response);
-        } else {
-          console.log(event);
-          const response = {
-            statusCode: 200,
-            headers: {
-              "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-              "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-            },
-            body: JSON.stringify({ "message": "Request submitted! Expect an email within a couple of days." })
-          };
-          callback(null, response);
-        }
+      Message: 'Send result of ' + requestBody.expression + ' to ' + requestBody.email,
+      TopicArn: process.env.TOPIC_ARN
+    }).promise().then(function(data) {
+      console.log(event);
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+          "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+        },
+        body: JSON.stringify({ "message": "Request submitted! Expect an email within a couple of days." })
+      };
+      callback(null, response);
+    }).catch(function(err) {
+      console.log(err.stack);
+      const response = {
+        statusCode: 503,
+        headers: {
+          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+          "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+        },
+        body: JSON.stringify({ "message": "Server error " + err.stack })
+      };
+      callback(null, response);
     });
-
+  } else {
+    const response = {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({ "message": "invalid request" })
+    };
+    callback(null, response);
+  }
 };
